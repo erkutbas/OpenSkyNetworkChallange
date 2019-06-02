15be36c5a7fad93017e81dc9e819bd4049158cdc
erkutbas <ebas1@binghamton.edu>
2019-06-02 05:14:32 +0300

    Initial Commit

---
+++ iOS_OpenSky_BiTaksi_Challange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_OpenSky_BiTaksi_Challange
+//
+//  Created by Erkut Baş on 6/2/19.
+//  Copyright © 2019 Erkut Baş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
